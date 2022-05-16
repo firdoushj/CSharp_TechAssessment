ea78c71c5eee8677091aa55d862662b4d9a75861
Firdoush Jamil <firdoushjamil95@gmail.com>
2022-05-16 21:55:22 +0530

    Segregating models to follow Single Responsibility Principle

---
+++ ExamApp/Model/Course.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ExamApp
+{
+    public class Course
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public virtual ICollection<Student> Students { get; set; }
+        public virtual Language Language { get; set; }
+    }
+}
